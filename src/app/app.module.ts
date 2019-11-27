311b1bf605d3c2bbaf8f3bade690362a2a158020
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2019-11-27 00:12:44 -0300

    Subindo primeiras features da tela home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainAreaComponent } from './main-area/main-area.component';
+import { OffertsComponent } from './main-area/offerts/offerts.component';
+import { IconButtonsComponent } from './header/icon-buttons/icon-buttons.component';
+import { ButtonLoginComponent } from './header/icon-buttons/button-login/button-login.component';
+import { SingUpComponent } from './header/icon-buttons/button-login/sing-up/sing-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainAreaComponent,
+    OffertsComponent,
+    IconButtonsComponent,
+    ButtonLoginComponent,
+    SingUpComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/icon-buttons/icon-buttons.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-buttons',
+  templateUrl: './icon-buttons.component.html',
+  styleUrls: ['./icon-buttons.component.css']
+})
+export class IconButtonsComponent implements OnInit {
+  public isOpenLogin = false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public open(value: boolean){
+    return value = !value;
+  }
+}
