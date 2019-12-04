694d8af972057681840a8b7c8ffd40afa1bd305a
GuilhermeLimaSoares <guilhermelimasoares9@gmail.com>
2019-12-04 09:09:42 -0300

    Crinado lista de desejos

---
+++ src/services/favorites.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FavoritesService {
+  public favorites = new BehaviorSubject(undefined);
+  public favoriteCollection = [];
+  constructor() { }
+
+  public getFavorites(obj): void {
+    this.favoriteCollection = this.favoriteCollection.concat(obj);
+
+    if (this.favoriteCollection.length > 0) {
+      this.favorites.next(this.favoriteCollection);
+    }
+  }
+}
